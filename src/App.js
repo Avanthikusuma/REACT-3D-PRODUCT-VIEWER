654989d8b54e1c5744df2c6ee4115c549303b918
Avanthikusuma <kusumaavanthi57@gmail.com>
2026-05-29 20:58:36 +0530

    Create App component for 3D product viewing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductViewer from './components/ProductViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 3D Product Viewer</h1>
+      <ProductViewer />
+    </div>
+  );
+}
+
+export default App;
