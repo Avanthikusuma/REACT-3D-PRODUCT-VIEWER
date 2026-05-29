513d8e3b3573b152f4cf7e2a7f336c5fc5b6cc74
Avanthikusuma <kusumaavanthi57@gmail.com>
2026-05-29 21:01:43 +0530

    Create ProductViewer.js

---
+++ src/components/ProductViewer.js
@@ -0,0 +1,13 @@
+function ProductViewer() {
+  return (
+    <div>
+      <img
+        src="https://via.placeholder.com/300"
+        alt="Product"
+      />
+      <p>3D Product Preview</p>
+    </div>
+  );
+}
+
+export default ProductViewer;
